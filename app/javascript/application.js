39b44e1e83321cfc3f2b54b715e276896d777c2e
Valdemar <hersones21@gmail.com>
2023-02-07 23:42:43 +0300

    Finish log in/log out

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require turbolinks
-//= require_tree .
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,4 +11,33 @@ class User < ApplicationRecord
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Возвращает дайджест данной строки.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Возвращает случайный токен.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в базе данных для использования в постоянной сессии.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Возвращает true, если предоставленный токен совпадает с дайджестом.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Забывает пользователя
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
